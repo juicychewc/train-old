3f1755f857d4517ac4916e99e034c1e4c37e5cd8
Sushichan <collin.chew.7188@gmail.com>
2019-05-01 10:25:02 -0700

    src setup

--- server.js
+++ server.js
@@ -5,5 +5,27 @@ const mongoose = require("mongoose");
 const routes = require("./routes");
 const app = express();
 const PORT = process.env.PORT || 3001;
-const session = require("express-session");
-const passport = require("./config/passport");
+
+// Add routes, both API and view
+app.use(routes);
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.get("*", function(req, res) {
+    const index = path.join(__dirname, "build", ".public/index.html");
+    res.sendFile(index);
+  });  
+
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/chewchoomongo");
+
+// Start the API server
+app.listen(PORT, function() {
+    console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
+  });
+  
